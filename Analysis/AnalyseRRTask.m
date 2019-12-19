0f5e4d9c613e3afd3458a9624a02ec34e3f86c07
LisaKirchberger <kirchberger.lisa@googlemail.com>
2019-12-19 17:03:02 +0100

    added testphase analysis to RRTask

--- Analysis/AnalyseRRTask.m
+++ Analysis/AnalyseRRTask.m
@@ -1,7 +1,7 @@
 %% RRTask Analysis
 
 clearvars
-%close all
+close all
 clc
 
 %%
@@ -170,4 +170,143 @@ ylabel('dprime')
 title(sprintf('Learning Progress Phase 2 %s', Mousename))
 
 
+%% Test Phase 3 or 4 or 5
 
+% have to correct sth, in the first Session Dropbox was turned off, so
+% conditions 5 and 6 were accidentally sometimes NoGo trials, correct this!
+% the wrong Logfile was called {Bambi_20191217_B1}, change all CR into Miss
+% and all FA into Hit
+% change all CR into Hits
+wrongCRTrials = find(strcmp(cellstr(CombinedTable.Logfile_name), 'Bambi_20191217_B1') & ~isnan(CombinedTable.TestStim) & CombinedTable.Reactionidx == 2);
+for i = 1:length(wrongCRTrials)
+    CombinedTable.Reactionidx(wrongCRTrials(i)) = 0;
+end
+wrongFATrials = find(strcmp(cellstr(CombinedTable.Logfile_name), 'Bambi_20191217_B1') & ~isnan(CombinedTable.TestStim) & CombinedTable.Reactionidx == -1);
+for i = 1:length(wrongFATrials) 
+    CombinedTable.Reactionidx(wrongFATrials(i)) = 1;
+end
+
+for t = [1 2 5 6]
+    wantedTrials = find(CombinedTable.Passives == 0 & CombinedTable.TestStim ==t);
+    Resp_rate_test(t) = sum(CombinedTable.Reactionidx(wantedTrials)==1)/length(wantedTrials);
+    TestResp{t} = CombinedTable.Reactionidx(wantedTrials);
+end
+
+for t = [3 4]
+    wantedTrials = find(CombinedTable.Passives == 0 & CombinedTable.TestStim ==t);
+    Resp_rate_test(t) = sum(CombinedTable.Reactionidx(wantedTrials)==-1)/length(wantedTrials);
+    TestResp{t} = CombinedTable.Reactionidx(wantedTrials);
+end
+
+
+% %% or
+% 
+% for t = [1 2 5 6]
+%     wantedTrials = find(CombinedTable.Passives == 0 & CombinedTable.TestStim ==t & CombinedTable.TaskPhase == 5);
+%     Resp_rate_test(t) = sum(CombinedTable.Reactionidx(wantedTrials)==1)/length(wantedTrials);
+%     TestResp{t} = CombinedTable.Reactionidx(wantedTrials);
+% end
+% 
+% for t = [3 4]
+%     wantedTrials = find(CombinedTable.Passives == 0 & CombinedTable.TestStim ==t & CombinedTable.TaskPhase == 5);
+%     Resp_rate_test(t) = sum(CombinedTable.Reactionidx(wantedTrials)==-1)/length(wantedTrials);
+%     TestResp{t} = CombinedTable.Reactionidx(wantedTrials);
+% end
+
+
+%% show the Hit/Miss/FA/CR distribution
+figure
+hold all
+for t = 1:6
+    for i = 1:length(TestResp{t})
+        switch TestResp{t}(i)
+            case 1 %Hit
+                plot(i,t, 's','MarkerFaceColor', [0 1 0], 'MarkerEdgeColor', 'k', 'MarkerSize', 12)
+            case 0 %Miss
+                plot(i,t, 's','MarkerFaceColor', [1 0.5 0.3], 'MarkerEdgeColor', 'k', 'MarkerSize', 12)
+            case 2 %CR
+                plot(i,t, 's','MarkerFaceColor', [0 1 1], 'MarkerEdgeColor', 'k', 'MarkerSize', 12)
+            case -1 %FA
+                plot(i,t, 's','MarkerFaceColor', [1 0 0], 'MarkerEdgeColor', 'k', 'MarkerSize', 12)
+        end
+    end
+end
+box off
+set(gca, 'TickDir', 'out')
+xlabel('Trial')
+ylabel('Test Stimulus')
+ylim([0 7])
+yticks(1:6)
+title(sprintf('Test Stimuli %s', Mousename))
+
+%% plot the d-prime for the TestPhase 
+
+windowsize = 50;
+wantedTrials = find(CombinedTable.Passives == 0 & CombinedTable.TaskPhase > 2 & isnan(CombinedTable.TestStim));
+wanted_Responses = CombinedTable.Reactionidx(wantedTrials);
+dprime_testing = [];
+for i = 1:length(wantedTrials)
+    if i <= windowsize/2
+        dprime_testing(i) = Calcdprime(wanted_Responses(1:i+windowsize/2-1));
+    elseif i > length(wanted_Responses)-windowsize/2
+        dprime_testing(i) = Calcdprime(wanted_Responses(i-windowsize/2:end));
+    else
+        dprime_testing(i) = Calcdprime(wanted_Responses(i-windowsize/2:i+windowsize/2-1));
+    end
+end
+figure('Position', [82    47   471   315])
+plot(dprime_testing, 'k-', 'LineWidth', 1)
+axis tight
+hold on
+plot(get(gca, 'XLim'),[0 0],'r')
+plot(get(gca, 'XLim'),[1.5 1.5],'g')
+cumTrials = 0;
+for i = unique(CombinedTable.SessID(wantedTrials))'
+    SessTrials = length(find(CombinedTable.SessID(wantedTrials) == i));
+    plot([cumTrials+SessTrials cumTrials+SessTrials], get(gca, 'YLim'), 'k--', 'LineWidth', 0.5)
+    cumTrials = cumTrials+SessTrials;
+end
+box off
+set(gca, 'TickDir', 'out')
+xlabel('Trials')
+ylabel('dprime')
+title(sprintf('Accuracy Test Phase %s', Mousename))
+
+
+% overall d-prime
+dprime_testing_all = Calcdprime(wanted_Responses);
+Hitrate_all = sum(CombinedTable.Reactionidx(wantedTrials)==1)/(sum(CombinedTable.Reactionidx(wantedTrials)==1)+sum(CombinedTable.Reactionidx(wantedTrials)==0));
+FArate_all = sum(CombinedTable.Reactionidx(wantedTrials)==-1)/(sum(CombinedTable.Reactionidx(wantedTrials)==-1)+sum(CombinedTable.Reactionidx(wantedTrials)==2));
+
+%% 
+if 0
+   % create and save the visual stimuli of this mouse as bmps
+   global Par
+   Log.Mouse = Mousename;
+   run checkMouse
+   run Params_Boxes
+   Par.ScreenID = 0;
+   cgloadlib
+   cgshut
+   cgopen(Par.Screenx, Par.Screeny, 32,Par.Refresh , Par.ScreenID)
+   cogstd('sPriority','high')
+   run makeVisStimsprites
+   Filepath = 'D:\Dropbox\19.18.07 RRTask\VisualStimuli';
+   ESC=0;
+   while ~ESC
+       Fgsprite = input('Input Figure Sprite \n');
+       Bgsprite = input('Input Background Sprite \n');
+       cgdrawsprite(Fgsprite,0,0)
+       cgdrawsprite(Bgsprite,0,0)
+       cgflip(Par.grey)
+       Filename = [Filepath, '\', Mousename, '_', num2str(Fgsprite), '_', num2str(Bgsprite)];
+       cgscrdmp(Filename)
+       [kd,kp] = cgkeymap;
+       if length(find(kp)) == 1
+           if find(kp) == 1
+               ESC = 1;
+           end
+       end
+   end
+   cgshut
+end
--- RRTask_testme.m
+++ RRTask_testme.m
@@ -232,6 +232,14 @@ try
                     Stims = Stims(randperm(length(Stims)));
                     TestMatrix = [TestMatrix Stims]; %#ok<AGROW>
                 end
+            elseif Log.TaskPhase(Trial) == 5
+                TestStim = 1:6;
+                TestStim = TestStim(randperm(length(TestStim)));
+                for t = 1:length(TestStim)
+                    Stims = [NaN(1,3) TestStim(t)]; % 75% Normal Trials, 25% Test Trials
+                    Stims = Stims(randperm(length(Stims)));
+                    TestMatrix = [TestMatrix Stims]; %#ok<AGROW>
+                end
             end
         end
 
@@ -259,7 +267,7 @@ try
             elseif Log.TestStim(Trial) == 3 || Log.TestStim(Trial) == 4     % 3 NoGo    Figure grating, Background grey
                 Log.Trialtype(Trial) = 0;                                   % 4 NoGo    Figure grey, Background grating
             elseif Log.TestStim(Trial) == 5 || Log.TestStim(Trial) == 6     % 5 random reward Go Fig + NoGo Bg 
-                Log.Trialtype(Trial) = randi([0 1], 1);                     % 6 random reward NoGo Fig + Go Bg
+                Log.Trialtype(Trial) = 1;%randi([0 1], 1);                     % 6 random reward NoGo Fig + Go Bg
             else 
                 Log.Trialtype(Trial) = TrialMatrix(1);                      % NaN       is a normal trial, just take next normal trial from trial matrix
                 TrialMatrix(1) = [];
